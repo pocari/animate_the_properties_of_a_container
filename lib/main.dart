fb3547318daee12619a9789c2d7c0bb5d05c5ae0
pocari <caffelattenonsugar@gmail.com>
2019-11-23 16:28:28 +0900

    start the animation by rebuilding with new properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,58 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AnimatedContainerApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class AnimatedContainerApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AnimateContainerAppState();
+}
+
+class _AnimateContainerAppState extends State<AnimatedContainerApp> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Test"),
+        ),
+        body: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          duration: Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () {
+            setState(
+              () {
+                final random = Random();
+                _width = random.nextInt(300).toDouble();
+                _height = random.nextInt(300).toDouble();
+                _color = Color.fromRGBO(
+                  random.nextInt(256),
+                  random.nextInt(256),
+                  random.nextInt(256),
+                  1,
+                );
+                _borderRadius =
+                    BorderRadius.circular(random.nextInt(100).toDouble());
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 }
